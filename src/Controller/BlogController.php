494d4bf870a86f8ac57e3ebe6d108ee4b5fd15a8
Fakum BACCAM <fakum.baccam@external.engie.com>
2020-10-31 22:14:32 +0100

    Add blog display

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,27 +4,32 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\BlogRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function blogList()
+    public function blogList(BlogRepository $rep)
     {
+        $array = $rep->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'blogs' => $array,
         ]);
     }
 
     /**
      * @Route("/blog/{slug}", name="blog_render",)
      */
-    public function blogRender(BlogRepository $rep,$slug) {
+    public function blogRender($slug,BlogRepository $rep) {
         $blog = $rep->findBySlug($slug);
 
         return $this->render('blog/blog.html.twig', [
             'controller_name' => 'BlogController',
+            'blog' => $blog,
         ]);
     }
 }
