132ccd5090128533dc463f45173060e1f27663dd
Fakum Baccam <faokiam@gmail.com>
2020-12-01 16:41:35 +0100

    Add Restriction to role admin for redactor pages

--- src/Form/PartnerFormType.php
+++ src/Form/PartnerFormType.php
@@ -6,6 +6,7 @@ use App\Entity\Partner;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PartnerFormType extends AbstractType
 {
@@ -15,6 +16,7 @@ class PartnerFormType extends AbstractType
             ->add('name')
             ->add('link')
             ->add('icon')
+            ->add('save', SubmitType::class, ['label' => 'Valider'])
         ;
     }
 
