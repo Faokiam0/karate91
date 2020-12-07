0688c1286e982e0529de687fbc025aebde6f50ac
FaokiamZero <faokiam@gmail.com>
2020-12-07 16:46:47 +0100

    Password reset

--- src/Controller/RedactorController.php
+++ src/Controller/RedactorController.php
@@ -4,16 +4,71 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Form\ChangePasswordType;
 
 class RedactorController extends AbstractController
 {
+    
+     public static function passwordValidator(String $str) : bool {
+        //check longueur
+        if (strlen($str) < 8 || strlen($str) >16)
+            return false;
+        // Validate password strength
+        $uppercase = preg_match('@[A-Z]@', $str); //check Majuscule
+        $lowercase = preg_match('@[a-z]@', $str); //check Minuscule
+        $number    = preg_match('@[0-9]@', $str); //check contient un numéro
+        $specialChars = preg_match('@[^\w]@', $str); //check char spéciaux
+        if(!$uppercase || !$lowercase || !$number || !$specialChars)
+            return false;
+
+        return true;
+    }
+
     /**
      * @Route("/r3d4ct0r", name="redactor")
      */
-    public function index()
+    public function index(Request $request, UserPasswordEncoderInterface $encoder)
     {
+        $nope = $this->passwordValidator("PASDEMINUS0");
+        $yep = $this->passwordValidator("Valide.2020");
+
+
+        $user =  $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class);
+        $form->handleRequest($request);
+        $error=false;
+        if($form->isSubmitted()){
+            $data = $form->getData();
+            $newpassword = $data['newpassword'];
+            $newpassword2 = ($data['newpassword2'] == $data['newpassword']);
+            $oldpassword = $encoder->isPasswordValid($user,$data['oldpassword']);
+
+            if($form->isValid() && $oldpassword && $newpassword2 && $this->passwordValidator($newpassword)){
+                $user->setPassword($encoder->encodePassword($user, $newpassword));
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($user);
+                $manager->flush();
+                
+                return $this->redirectToRoute('redactor');
+            }
+            else{
+                $error = "Pas bon";
+
+            }
+
+            
+        }
+
+
         return $this->render('redactor/index.html.twig', [
             'controller_name' => 'RedactorController',
+            'form' => $form->createView(),
+            'error' => $error,
+            'yep' => $yep,
+            'nope' => $nope,
         ]);
     }
 }
---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('oldpassword', PasswordType::class)
+            ->add('newpassword', PasswordType::class)
+            ->add('newpassword2', PasswordType::class)
+        ;
+    }
+}
