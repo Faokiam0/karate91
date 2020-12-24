841247b8df0c16609c8ce2060b0442632f026e3c
Formation-01 <faokiam@gmail.com>
2020-12-24 11:16:17 +0100

    Add info templates

--- src/Controller/InformationController.php
+++ src/Controller/InformationController.php
@@ -11,11 +11,37 @@ class InformationController extends AbstractController
     /**
      * @Route("/information/planning", name="planning")
      */
-    public function index(): Response
+    public function planning(): Response
     {
        // $planning = "";
 
         return $this->render('information/planning.html.twig', [
+            'controller_name' => 'InformationController',
+            //'planning' => $planning,
+        ]);
+    }
+
+    /**
+     * @Route("/information/tarif", name="tarif")
+     */
+    public function tarif(): Response
+    {
+       // $planning = "";
+
+        return $this->render('information/tarif.html.twig', [
+            'controller_name' => 'InformationController',
+            //'planning' => $planning,
+        ]);
+    }
+
+    /**
+     * @Route("/information/plan", name="plan")
+     */
+    public function plan(): Response
+    {
+       // $planning = "";
+
+        return $this->render('information/plan.html.twig', [
             'controller_name' => 'InformationController',
             //'planning' => $planning,
         ]);
